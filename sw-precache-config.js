a63d9e6449dc2eab79422994b7e87ccabd10418e
LadyHail <ladydeath@o2.pl>
2018-05-16 22:55:31 +0200

    SW fixed

--- sw-precache-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  staticFileGlobs: [
-    './build/**/**.html',
-    './build/static/**',
-  ],
-  dontCacheBustUrlsMatching: /\.\w{8}\./,
-  swFilePath: './build/service-worker.js'
-}
